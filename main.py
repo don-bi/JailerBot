838cbd2abf5cf9bbb63b164bb5954d1a8a85a7ce
Donald <78169663+don-bi@users.noreply.github.com>
2021-10-11 01:15:25 +0000

    finished imprison command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.utils import get
 import os
 
 client = commands.Bot(command_prefix = '|')
@@ -23,9 +24,39 @@ async def on_message(message):
 
   await client.process_commands(message)
 
-@client.command()
-async def imprison(ctx, member : discord.Member, hour):
-  await ctx.channel.send(f'@{member} has been imprisoned for {hour}.')
+def findhighestrole(ctx, member):
+  highestrole = ''
+  for roles in ctx.guild.roles:
+    for roles2 in member.roles:
+      if roles2 == roles:
+        highestrole = roles2
+  return highestrole
+
+@client.command(pass_context=True)
+async def imprison(ctx, member : discord.Member):
+  if findhighestrole(ctx, ctx.author) > findhighestrole(ctx, member):
+    memberrole = get(ctx.guild.roles, name='member')
+    role = get(ctx.guild.roles, name='.')
+    stuy = get(ctx.guild.roles, name='stuy')
+    await member.remove_roles(memberrole)
+    await member.remove_roles(role)
+    await member.remove_roles(stuy)
+    await ctx.channel.send(f'@{member} has been imprisoned.')
+  else:
+    await ctx.channel.send(f'Your permissions are not high enough to imprison @{member}.')
+
+@client.command(pass_context=True)
+async def free(ctx, member : discord.Member):
+  if findhighestrole(ctx, ctx.author) > findhighestrole(ctx, member):
+    memberrole = get(ctx.guild.roles, name='member')
+    role = get(ctx.guild.roles, name='.')
+    stuy = get(ctx.guild.roles, name='stuy')
+    await member.add_roles(memberrole)
+    await member.add_roles(role)
+    await member.add_roles(stuy)
+    await ctx.channel.send(f'@{member} has been freed.')
+  else:
+    await ctx.channel.send(f'Your permissions are not high enough to free @{member}.')
 
 
 client.run(os.environ['TOKEN'])
